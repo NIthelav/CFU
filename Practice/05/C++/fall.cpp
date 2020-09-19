ea79961421ce1e20bc70045987fc7b442045105c
Nithelav <ghostinhosts@gmail.com>
2020-09-19 11:17:25 +0300

    Added task 5

--- Practice/05/C++/fall.cpp
+++ Practice/05/C++/fall.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+template <typename Num>
+
+Num input(string msg)
+{
+    Num var;
+    cout << msg;
+    cin >> var;
+    cout << endl;
+    return var;
+}
+
 int main()
 {
-    
+    const float G = 9.8;
+    double x0, v0;
+    int t;
+    x0 = input<double>("Input x0: ");
+    v0 = input<double>("Input v0: ");
+    t = input<int>("Input t: ");
+
+    cout << "Distance = " << x0 + v0 * t + G * t * t / 2 << endl;
 }
---
+++ Practice/05/Python/fall.py
@@ -0,0 +1,7 @@
+G = 9.8
+
+x0 = float(input("Input x0: "))
+v0 = float(input("Input v0: "))
+t = int(input("Input t: "))
+
+print(x0 + v0 * t + G * t * t / 2)
